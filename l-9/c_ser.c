50f60888c91fe043443699e7de3a9f9083687ae5
sriram <71762205115@cit.edu.in>
2024-10-15 11:26:55 +0530

    comms

--- l-9/c_ser.c
+++ l-9/c_ser.c
@@ -7,14 +7,14 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-// Function to perform CRC check
+
 void calculateCRC(char *message, char *crc) {
-    // Divisor polynomial: x^3 + 1 -> 1001
+    
     char divisor[] = "1001";
     int messageLength = strlen(message);
     int divisorLength = strlen(divisor);
     
-    // Append zeros to the original message
+    
     char *appendedMessage = malloc(messageLength + divisorLength - 1);
     strcpy(appendedMessage, message);
     for (int i = 0; i < divisorLength - 1; i++) {
@@ -22,7 +22,7 @@ void calculateCRC(char *message, char *crc) {
     }
     appendedMessage[messageLength + divisorLength - 1] = '\0';
 
-    // Binary division
+    
     for (int i = 0; i < messageLength; i++) {
         if (appendedMessage[i] == '1') {
             for (int j = 0; j < divisorLength; j++) {
@@ -31,7 +31,7 @@ void calculateCRC(char *message, char *crc) {
         }
     }
 
-    // Extract CRC (last 3 bits)
+    
     strcpy(crc, appendedMessage + messageLength);
     free(appendedMessage);
 }
@@ -44,13 +44,13 @@ int main() {
     char buffer[BUFFER_SIZE] = {0};
     char crc[4];
 
-    // Create socket
+    
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Bind the socket
+    
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -64,7 +64,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Listen for connections
+    
     if (listen(server_fd, 3) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
@@ -76,22 +76,22 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Read message from client
+    
     read(new_socket, buffer, BUFFER_SIZE);
     printf("Received message: %s\n", buffer);
 
-    // Calculate CRC
+    
     calculateCRC(buffer, crc);
     printf("Calculated CRC: %s\n", crc);
 
-    // Check for errors (the received CRC should match the calculated CRC)
+    
     if (strcmp(crc, "000") == 0) {
         printf("No errors detected.\n");
     } else {
         printf("Errors detected!\n");
     }
 
-    // Close sockets
+    
     close(new_socket);
     close(server_fd);
     return 0;
